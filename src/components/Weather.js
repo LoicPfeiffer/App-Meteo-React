497ccee81b916b2f594815e77c9b3e7a5a940184
LoicPfeiffer <95574385+LoicPfeiffer@users.noreply.github.com>
2022-04-12 22:10:14 +0200

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import { Button } from "semantic-ui-react";
+
 import moment from "moment";
 
 const refresh = () => {
@@ -9,30 +9,28 @@ const refresh = () => {
 
 const WeatherCard = ({ weatherData }) => (
   <div className="main">
+    <h2>Application Méteo</h2>
     <p className="header">🏠 Nom de la ville :{weatherData.city.name}</p>
-    📅 Date: {moment().format("dddd")} ⌚{moment().format("LL")}
-    <div className="top">
-      <Button
-        className="button"
-        inverted
-        color="blue"
-        circular
-        icon="refresh"
-        onClick={refresh}
-      />
-    </div>
-    <p className="temp">Code Postal : {weatherData.city.cp}</p>
+    <p className="tump">
+      📅 Date: {moment().format("dddd")} 🌱
+      {moment().format("LL")}⌚{moment().format("h:mm:ss a")}
+    </p>
+    <p className="temp">📮Code Postal : {weatherData.city.cp}</p>
     <p className="temp">🔍 Latitude : {weatherData.ephemeride.latitude}</p>
     <p className="temp">🔎 Longitude : {weatherData.ephemeride.longitude}</p>
     {/*<div>{JSON.stringify(weatherData, undefined, true)}</div>*/}
-    <div className="flex">
-      <p className="temp">🙋🙋‍♀️Lever : {weatherData.ephemeride.sunrise} h</p>
-      <p className="temp">🛌 Coucher: {weatherData.ephemeride.sunset} h</p>
-      <p className="temp">
-        🌗 Durée du Jour: {weatherData.ephemeride.duration_day} h
-      </p>
-      <p className="temp">🌙 Lune: {weatherData.ephemeride.moon_phase}</p>
-    </div>
+    <p className="temp">🙋🙋‍♀️Lever : {weatherData.ephemeride.sunrise} h</p>
+    <p className="temp">🛌 Coucher: {weatherData.ephemeride.sunset} h</p>
+    <p className="temp">
+      🌗 Durée du Jour: {weatherData.ephemeride.duration_day} h
+    </p>
+    <p className="temp">🌙 Lune: {weatherData.ephemeride.moon_phase}</p>
+    <button class="raf" onClick={refresh}>
+      Actualiser
+    </button>
+    <button class="raf" onClick={refresh}>
+      Actualiser
+    </button>
   </div>
 );
 
